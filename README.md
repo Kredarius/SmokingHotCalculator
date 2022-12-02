3db006cf0e24b1a3f95368ddc08e858db987e2ae
Kredarius <95450518+Kredarius@users.noreply.github.com>
2022-12-02 16:34:47 +0100

    Added CSS, removed button

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SmokingHotCalculator
 Simple web calculator to calculate how much money is spend for cigarets
+
+    <iframe src="https://kredarius.github.io/SmokingHotCalculator/" width="500" height="300"></iframe>
--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 
 function calculate() {
-	let cost = +document.getElementById("cost").value;
+	  let cost = +document.getElementById("cost").value;
     let consumPerDay = +document.getElementById("per-day").value;
+		if (cost > 999) {
+			cost = 999;
+			document.getElementById("cost").value = cost;
+		}
+		if (consumPerDay > 999) {
+			consumPerDay = 999;
+			document.getElementById("per-day").value = consumPerDay;
+		}
     let costPerDay = cost * consumPerDay / 20;
     let costPerMonth = costPerDay * 30;
     let costPerYear = costPerDay * 365;
     let costPer10Years = costPerDay * 3652;
-    document.getElementById("month").innerHTML = costPerMonth;
-    document.getElementById("year").innerHTML = costPerYear;
-    document.getElementById("10years").innerHTML = costPer10Years;
+		function format(num) {
+			text = Math.floor(num).toString();
+			return (text.slice(0,-6)+" "+text.slice(-6,-3)+" "+text.slice(-3))
+		}
+    document.getElementById("month").innerHTML = format(costPerMonth);
+    document.getElementById("year").innerHTML = format(costPerYear);
+    document.getElementById("10years").innerHTML = format(costPer10Years);
 }
+document.getElementById("cost").addEventListener("change", calculate)
+document.getElementById("per-day").addEventListener("change", calculate)
